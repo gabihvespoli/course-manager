c0c04e8c5a4bcc48acd9045a5d357758badf5f78
Gabriela Vespoli <gabiat3@hotmail.com>
2021-07-27 21:48:40 -0300

    implements navbar and routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,6 @@
+import { Error404Component } from './error-404/error-404.component';
+import { NavBarComponent } from './navbar/nav-bar.component';
+import { ReplacePipe } from './pipe/replace.pipe';
 import { StarComponent } from './star/star.component';
 import { CourseListComponent } from './courses/course-list.component';
 import { NgModule } from '@angular/core';
@@ -5,16 +8,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent,
     CourseListComponent,
-    StarComponent
+    StarComponent,
+    ReplacePipe,
+    NavBarComponent,
+    Error404Component,
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    RouterModule.forRoot([
+      { path: '', redirectTo: 'courses', pathMatch: 'full' },
+      { path: 'courses', component: CourseListComponent },
+      { path: '**', component: Error404Component },
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
