943694c804d86c707c21bfdae42b4888160ebe34
Gabriela Vespoli <gabiat3@hotmail.com>
2021-08-01 17:15:11 -0300

    feat: shared folder and implementations

--- src/app/courses/course.module.ts
+++ src/app/courses/course.module.ts
@@ -1,22 +1,22 @@
+import { AppPipeModule } from './../shared/pipe/app-pipe.module';
+import { StarModule } from './../shared/components/star/star.module';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { CourseInfoComponent } from './course-info.component';
 import { CourseListComponent } from './course-list.component';
 import { NgModule } from "@angular/core";
-import { ReplacePipe } from '../pipe/replace.pipe';
-import { StarComponent } from '../star/star.component';
 import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
     CourseListComponent,
     CourseInfoComponent,
-    ReplacePipe,
-    StarComponent,
   ],
   imports: [
     CommonModule,
     FormsModule,
+    StarModule,
+    AppPipeModule,
     RouterModule.forChild([
       { path: 'courses', component: CourseListComponent },
       { path: 'courses/info/:id', component: CourseInfoComponent },
