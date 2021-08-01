3a876ff7854dd5bf33d71e898822f0dd1b25fb61
Gabriela Vespoli <gabiat3@hotmail.com>
2021-08-01 16:27:56 -0300

    feat: add delete method

--- src/app/courses/course.service.ts
+++ src/app/courses/course.service.ts
@@ -42,4 +42,8 @@ export class CourseService {
       return this.httpClient.post<Course>(`${this.courseUrl}`, course);
     }
   }
+
+  deleteById(id: number): Observable<any> {
+    return this.httpClient.delete<any>(`${this.courseUrl}/${id}`);
+  }
 }
