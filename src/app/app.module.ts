e15b2850eedc4e3f97655dca26fa0aee5f00ca24
Gabriela Vespoli <gabiat3@hotmail.com>
2021-08-01 17:24:08 -0300

    feat: core folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
+import { CoreModule } from './core/core.module';
 import { CourseModule } from './courses/course.module';
 import { Error404Component } from './error-404/error-404.component';
-import { NavBarComponent } from './navbar/nav-bar.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
@@ -11,13 +11,13 @@ import { AppComponent } from './app.component';
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent,
     Error404Component,
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     CourseModule,
+    CoreModule,
     RouterModule.forRoot([
       { path: '', redirectTo: 'courses', pathMatch: 'full' },
       { path: '**', component: Error404Component },
